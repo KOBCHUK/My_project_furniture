01a3b5652835b4b0f30fd5d1f226cc51119cdc91
KOBCHUK <irravadi@gmail.com>
2024-10-04 16:22:58 +0300

    done

--- furniture/my_furniture/materials.py
+++ furniture/my_furniture/materials.py
@@ -1,5 +1,3 @@
-# materials.py
-
 # База даних матеріалів
 materials_db = {
     "дошка": {"розмір": "200x20 см", "ціна": 50, "щільність": 0.7},
@@ -7,14 +5,24 @@ materials_db = {
     "фурнітура": {"ціна": 10},
 }
 
+
 def calculate_materials_needed(product, material_requirements):
-    """Розрахунок необхідної кількості матеріалів для виробу."""
+    """Розрахунок необхідної кількості матеріалів для виробу.
+    :param product: назва виробу
+    :param material_requirements: словник з вимогами до матеріалів
+    :return: словник з необхідними матеріалами
+    """
     required_materials = {}
     for material, amount in material_requirements.items():
         if material in materials_db:
             required_materials[material] = amount
     return required_materials
 
+
 def optimize_cutting(material_size, required_size):
-    """Оптимізація розкрою матеріалів."""
+    """Оптимізація розкрою матеріалів.
+    :param material_size: розмір матеріалу
+    :param required_size: розмір необхідного вирізу
+    :return: кількість вирізів
+    """
     return material_size // required_size
